cdae5ca2d3953587197ea6d5e75dd20a4e3025ed
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-17 20:15:18 -0400

    Update index.sh

--- index.sh
+++ index.sh
@@ -9,7 +9,8 @@ npm start
 else 
 echo Sorry, looks like that you do not have vyond remastered yet. Installing.... 
 wget https://github.com/DavidB2007/vyond-remastered-public/archive/main.zip 
-unzip main.zip 
+unzip main.zip
+unlink main.zip
 echo Vyond Remastered is installed. Starting.... 
 cd vyond-remastered-public-main/wrapper 
 npm start
