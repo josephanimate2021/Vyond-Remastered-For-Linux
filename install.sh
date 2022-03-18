d5e89d27f353724e156dc41e1e466e6cd3b38d14
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-17 22:05:04 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+Installing Vyond Remastered.... 
+wget https://github.com/DavidB2007/vyond-remastered-public/archive/main.zip 
+unzip main.zip
+unlink main.zip
+echo Vyond Remastered is installed. Starting.... 
+cd vyond-remastered-public-main/wrapper 
+npm start
+fi
