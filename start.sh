0206af1fcd2fb17e86c025294a77dd58fe585594
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-17 20:09:08 -0400

    Update and rename start.sh to index.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-# Vyond Remastered Launcher
-# Launcher Creator: joseph the animator#2292
-# Project Runner: David’s Creation#2007
-echo Starting Up Vyond Remastered...
-cd vyond-remastered-public-main/wrapper
-npm start
