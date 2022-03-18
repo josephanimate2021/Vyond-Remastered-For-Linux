317212c85e5ddeaa37e47ee6f05bf98c5e309bf3
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-17 20:06:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Vyond Remastered Installer
+# Installer Creator: joseph the animator#2292
+# Project Runner: David’s Creation#2007
 echo Installing Vyond Remastered... 
 wget https://github.com/DavidB2007/vyond-remastered-public/archive/main.zip 
 unzip main.zip
