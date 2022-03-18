042f6fb4d021e36e5098d9b6b0848072210e0702
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-17 20:11:32 -0400

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,15 +1,15 @@
 # Vyond Remastered Launcher
-# Launcher Creator: joseph the animator#2292
-# Project Runner: David’s Creation#2007
-if [ -e vyond-remastered-public-main ]
+# Launcher Creator Is joseph the animator hashtag 2292
+# Project Runner is Davids Creation hashtag 2007
+if -e vyond-remastered-public-main
 then
-  echo "Looks like you have Vyond Remastered! Starting...."
+  echo Looks like you have Vyond Remastered! Starting....
   cd vyond-remastered-public-main/wrapper
   npm start
 else 
-echo "Sorry, looks like that you don't have vyond remastered yet. Installing...." 
+echo Sorry, looks like that you don\'t have vyond remastered yet. Installing.... 
 wget https://github.com/DavidB2007/vyond-remastered-public/archive/main.zip 
 unzip main.zip 
-echo "Vyond Remastered is installed! Starting...." 
+echo Vyond Remastered is installed! Starting.... 
 cd vyond-remastered-public-main/wrapper 
 npm start
