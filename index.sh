a5a4957c3ff290416b7b509f459e52fe1a116b1b
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-17 22:06:17 -0400

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,7 +1,6 @@
 # Vyond Remastered Launcher
 # Launcher Creator Is joseph the animator hashtag 2292
 # Project Runner is Davids Creation hashtag 2007
-title Vyond Remastered Launcher
 echo Starting Up Vyond Remastered...
 cd vyond-remastered-public-main/wrapper
 npm start
